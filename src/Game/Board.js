e53e6367a702a05bfc1899508552e0216b28ae48
Baesiann <k3nn3thburk3@gmail.com>
2026-03-11 16:57:43 -0400

    pieces can now move through movePiece

--- src/Game/Board.js
+++ src/Game/Board.js
@@ -40,25 +40,58 @@ class Board {
         return this.Squares[column + (row - 1) * 8];
     }
 
-    // example:
-    setPiece(square, piece) {
+    // example: ("a4", Piece.color, Piece.Type)
+    // Sets a piece on a square with color and type
+    setPiece(square, pieceColor, pieceType) {
+        const rank_file = square.split("");
+        let column = this.rankDict[rank_file[0]];
+        let row = parseInt(rank_file[1]);
+    
+        // console.log(column, row);
+        // console.log(column + (row - 1) * 8);
+        this.Squares[column + (row - 1) * 8] = pieceColor | pieceType;
 
+        return this.Squares;
     }
 
-    init() {
-        // Convert to 2d array for viewing
-        var viewBoard = Array.from({ length: 8 }, () => new Array(8));
-        let board_index = 0;
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                viewBoard[i][j] = this.Squares[board_index];
-                board_index++;
-            }
-        }
-        console.log(viewBoard);
-        console.log(this.Squares);
-        console.log(this.getPiece("h1"));
+    // Sets a piece on a square by using it's int ID
+    setPieceByID(square, pieceInt) {
+        const rank_file = square.split("");
+        let column = this.rankDict[rank_file[0]];
+        let row = parseInt(rank_file[1]);
+    
+        // console.log(column, row);
+        // console.log(column + (row - 1) * 8);
+        this.Squares[column + (row - 1) * 8] = pieceInt;
+
+        return this.Squares;
     }
+
+    // example: ("a2", "a4")
+    movePiece(startSquare, endSquare) {
+        let pieceMoved = this.getPiece(startSquare);
+        this.setPieceByID(startSquare, 0);
+        this.setPieceByID(endSquare, pieceMoved);
+
+        return this.Squares;
+    }
+
+    // init() {
+    //     // Convert to 2d array for viewing
+    //     console.log(this.Squares);
+    //     // console.log(this.getPiece("h1"));
+    //     // console.log(this.setPiece("a4", Piece.Black, Piece.Bishop));
+    //     console.log(this.movePiece("a2", "a4"));
+    //     var viewBoard = Array.from({ length: 8 }, () => new Array(8));
+    //     let board_index = 0;
+    //     for (let i = 0; i < 8; i++) {
+    //         for (let j = 0; j < 8; j++) {
+    //             viewBoard[i][j] = this.Squares[board_index];
+    //             board_index++;
+    //         }
+    //     }
+    //     console.log(viewBoard);
+    // }
 }
 
 export { Board }
